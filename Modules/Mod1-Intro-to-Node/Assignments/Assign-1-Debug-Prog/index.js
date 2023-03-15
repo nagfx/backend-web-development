22ca983eb3f4b13cc0e8774fcd95bb81290f953f
Naman <89750281+nagfx@users.noreply.github.com>
2023-03-15 09:32:00 +0500

    Update index.js

--- Modules/Mod1-Intro-to-Node/Assignments/Assign-1-Debug-Prog/index.js
+++ Modules/Mod1-Intro-to-Node/Assignments/Assign-1-Debug-Prog/index.js
@@ -2,15 +2,18 @@
  * Complete `passTests`, so that all console logs will return true
  */
 function passTests() {
-  const var1 = null;
-  const var2 = null;
-  const var3 = null;
-  const var4 = null;
-  const var5 = null;
+  const var1 = "eleven";
+  const var2 = 4;
+  const var3 = 2;
+  const var4 = [1, 2, 3, 4];
+  const var5 = [5, 6, 7];
   console.log("var1 === 'eleven:'", var1 === "eleven");
   console.log("(var2 + var3) === 6", var2 + var3 === 6);
   console.log("(var2 * var3) === 8", var2 * var3 === 8);
-  console.log("Total sum of array 'var4' === 10", var4.reduce((p, c) => p + c) === 10);
+  console.log(
+    "Total sum of array 'var4' === 10",
+    var4.reduce((p, c) => p + c) === 10
+  );
   console.log(
     "var4.concat(var5) === [1, 2, 3, 4, 5, 6, 7]",
     var4.concat(var5).every((v) => [1, 2, 3, 4, 5, 6, 7].indexOf(v) >= 0)
@@ -29,7 +32,7 @@ function printTimeTaken() {
     I don't think it will take that long.
   `);
   // Calculate how long it took
-  const howLong = null;
+  const howLong = new Date() - now;
   console.log(`I took this long to run: ${howLong}secs`);
 }
 printTimeTaken();
